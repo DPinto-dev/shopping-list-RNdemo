df8c3740a1172dca7d9c3f1b81bc022dbdd14708
DPintoLL <diogosmp@gmail.com>
2020-06-29 23:55:03 -0700

    Basic components

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
+      <Image
+        source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}}
+        style={styles.img}
+      />
     </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {color: 'darkslateblue', fontSize: 30},
+  img: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+  },
+});
